78ad51720e58cb36d7c3a22ad2f6d0861cdc2e46
Nicolas Theau <theaun91@gmail.com>
2024-11-30 18:06:18 +0100

    routing app

--- covoiturage-app/src/app/app-routing.module.ts
+++ covoiturage-app/src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { userGuard } from './core/guards/user.guard';
+import { HomeComponent } from './components/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'login',
+    component: LoginComponent,
+  },
+  {
+    path: 'register',
+    component: LoginComponent,
+  },
+  {
+    path: "**",
+    component: HomeComponent,
+    canActivate: [userGuard],
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
