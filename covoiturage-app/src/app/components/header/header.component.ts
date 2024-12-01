6904bab3cf8a02effc07034df48d6708a9fb2ce3
Yanis Rozier <yanis.rozier@orange.fr>
2024-12-01 22:45:20 +0100

    Début CSS de l'accueil

--- covoiturage-app/src/app/components/header/header.component.ts
+++ covoiturage-app/src/app/components/header/header.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent {
-  isLoggedIn: boolean = true;
+  isLoggedIn: boolean = false;
   profileDropdown: boolean = false;
 
   toggleDropdown() {
