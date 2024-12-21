f16c5f20c29eaf4fc33fd0ee1d1a0552d4fe3cf7
Sebastien Zhou <Sebastien.4084@gmail.com>
2024-12-21 20:20:46 +0100

    Fix connexion

--- covoiturage-app/src/app/components/auth/login/login.component.ts
+++ covoiturage-app/src/app/components/auth/login/login.component.ts
@@ -10,6 +10,7 @@ import { Router } from '@angular/router';
 })
 export class LoginComponent implements OnInit {
   loginForm!: FormGroup;
+  errorMessage: string = '';
 
   constructor(
     private formBuilder: FormBuilder,
@@ -25,10 +26,21 @@ export class LoginComponent implements OnInit {
   }
 
   login() {
+    if (!this.loginForm.value.email || !this.loginForm.value.password) {
+      this.errorMessage = 'Veuillez remplir tous les champs';
+      return;
+    }
     this.authService.login(this.loginForm.value).subscribe((user: any) => {
-      if (user.length === 0) alert('User or password incorrect');
+      if (user.length === 0) {
+        this.errorMessage = 'Le nom d\'utilisateur ou le mot de passe est incorrect';
+        return;
+      }
       this.authService.user = user[0];
-      if (!this.authService.user) return;
+      if (!this.authService.user) {
+        this.errorMessage = 'Le nom d\'utilisateur ou le mot de passe est incorrect';
+        return;
+      }
+      this.errorMessage = '';
       this.authService.saveUser();
       this.router.navigate(['/']);
     });
